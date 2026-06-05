9d454a043b9003f8ae886aa0c6e4a68b988d24f6
puniee65-droid <puniee65@gmail.com>
2026-06-05 10:40:17 +0900

    Create datetime file on each run

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello World")
+from datetime import datetime
+
+now = datetime.now()
+filename = now.strftime("%Y-%m-%d_%H-%M-%S") + ".txt"
+content = now.strftime("%Y年%m月%d日 %H時%M分%S秒")
+
+with open(filename, "w", encoding="utf-8") as f:
+    f.write(content)
+
+print(f"Hello World - {content} を {filename} に書き込みました")
